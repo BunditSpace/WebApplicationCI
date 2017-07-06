644e2fbe47160a22828f07e053b20e02560bcf90
BunditSpace <spidar21@gmail.com>
2017-07-06 16:06:32 +0700

    add travis ci config to run unit test

---
+++ UnitTestProjectCI/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+using Assert = NUnit.Framework.Assert;
+
+namespace UnitTestProjectCI
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPassNumber()
+        {
+            int a = 5;
+            int b = 2;
+            int result = 7;
+
+            Assert.AreEqual(result, a + b);
+        }
+
+        [TestMethod]
+        public void TestMinusNumber()
+        {
+            int a = 5;
+            int b = 2;
+            int result = 3;
+
+            Assert.AreEqual(result, a - b);
+        }
+    }
+}
